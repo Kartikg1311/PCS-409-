390b2c83ea86b5377b0537bc15206436e2cb1a94
Kartik Gupta <140899352+Kartikg1311@users.noreply.github.com>
2024-02-03 15:07:49 +0530

    Update Ques003.cpp

--- WEEK 01/Ques003.cpp
+++ WEEK 01/Ques003.cpp
@@ -1,61 +1,64 @@
 #include<iostream>
 using namespace std;
-int find(int [],int ,int ); 
-void linear(int [],int ,int ,int );
+void find(int [],int ,int ); 
+void linear(int [],int ,int ,int ,int );
 int c=0;
 int main()
 {
-int n,m,k;
-cin>>n;
-while(n!=0)
-{
-cin>>m;
-int a[m];
-for(int i=0;i<m;i++)
-{
-cin>>a[i];
-}
-cin>>k;
-int f;
-f=find(a,m,k);
-if(f<0)
-cout<<"Not Preset"<<c<<endl;
-n--;
-}
-return 0;
+    int n,m,k;
+    cout<<"Enter the Number of Array:";
+    cin>>n;
+    while(n!=0)
+    {
+        cout<<"Enter the size of the Array:";
+        cin>>m;
+        int a[m];
+        for(int i=0;i<m;i++)
+        {
+            cout<<"Enter the "<<i+ 1<<" element:";
+            cin>>a[i];
+        }
+        cout<<"Enter the number:";
+        cin>>k;
+        find(a,m,k);
+        c=0;
+        n--;
+    }
+    return 0;
 }
 
-int find(int a[],int m,int k)
-{c++;
-if(a[0]==k)
+void find(int a[],int m,int k)
 {
-cout<<"Present"<<endl;
-return 0;
+    c++;
+    if(a[0]==k)
+    {
+        cout<<"Present "<<c<<endl;
+        return ;
+    }
+    else
+    {
+        int i;
+        for(i=1;i<m&&a[i]<=k;i=i*2)
+        {
+            c++;
+        }
+        linear(a,i/2,i,k,c- 1);
+    }
 }
-else
-{
-int i=1;
-while(i<m&&a[i]<=k)
-{
-i=i*2;
-}
-linear(a,i/2,m,k);
-}
-return 0;}
 
-void linear(int a[],int c, int b,int k)
+void linear(int a[],int lb,int ub,int k,int c)
 {
-int flag=0;
-for(int i=c;i<b;i++)
-{
-c++;
-if(a[i]==k)
-{
-flag=1;
-cout<<"Present";
-break;
-}
-}
-if(flag==0)
-cout<<"Not Present";
+    int flag=0;
+    for(int i=lb;i<ub;i++)
+    {
+        c++;
+        if(a[i]==k)
+        {
+            flag=1;
+            cout<<"Present "<<c<<endl;
+            break;
+        }
+    }
+    if(flag==0)
+    cout<<"Not Present "<<c<<endl;
 }
