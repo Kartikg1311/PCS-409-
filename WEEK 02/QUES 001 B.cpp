776ae41da3b0dc1b21fd87a9b9ed0e965a127e7c
Kartik Gupta <140899352+Kartikg1311@users.noreply.github.com>
2024-02-07 22:57:23 +0530

    Update QUES 001 B.cpp
    Given a sorted array of positive integers containing few duplicate elements, design an algorithm and implement it using a program to find whether the given key element is present in the array or not. If present, then also find the number of copies of given key. (Time Complexity = O(log n))
    Input format:
    The first line contains number of test cases, T.
    For each test case, there will be three input lines.
    First line contains n (the size of array).
    Second line contains space-separated integers describing array.
    Third line contains the key element that need to be searched in the array.
    Output format:
    The output will have T number of lines.
    For each test case T, output will be the key element and its number of copies in the array if the key element is present in the array otherwise print “Key not present”.

--- WEEK 02/QUES 001 B.cpp
+++ WEEK 02/QUES 001 B.cpp
@@ -1,1 +1,41 @@
+//using Lower bound and Upper bound function in Algorithm Library
+
+#include<iostream>
+#include<algorithm>
+using namespace std;
+void bound(int [],int ,int );
+int main()
+{
+    int n,m,k;
+    cout<<"Enter the number of the Array:";
+    cin>>n;
+    while(n!=0)
+    {
+        cout<<"Enter the size of the Array:";
+        cin>>m;
+        int a[m];
+        for(int i=0;i<m;i++)
+        {
+            cout<<"Enter the "<<i+ 1<<" element:";
+            cin>>a[i];
+        }
+        cout<<"Enter the number:";
+        cin>>k;
+        bound(a,m,k);
+        n--;
+    }
+    return 0;
+}
+
+void bound(int a[],int m,int k)
+{
+    int *r1=lower_bound(a,a+m,k);    //auto r1=lower_bound(a,a+m,k);
+    int *r2=upper_bound(a,a+m,k);    //auto r2=upper_bound(a,a+m,k);
+    int r=r2-r1;
+    if(r==0)
+    cout<<"Number not Found "<<r<<endl;
+    else
+    cout<<"Found "<<r<<endl;
+    return ;
+}
 
