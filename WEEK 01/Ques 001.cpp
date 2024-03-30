8a2c1eb4146789e2025c0f37c5e2673ee0f0cfae
Kartik Gupta <140899352+Kartikg1311@users.noreply.github.com>
2024-03-31 00:20:03 +0530

    Update Ques 001.cpp

--- WEEK 01/Ques 001.cpp
+++ WEEK 01/Ques 001.cpp
@@ -1,8 +1,9 @@
 #include<iostream>
+void linear(int [],int ,int );
 using namespace std;
 int main()
 {
-    int n,m,k,flag;
+    int n,m,k;
     cout<<"Enter the number of Array:";
     cin>>n;
     while(n!=0)
@@ -12,23 +13,25 @@ int main()
         int a[m];
         for(int i=0;i<m;i++)
         {
-            cout<<"Enter the"<<i+ 1<<" element:";
             cin>>a[i];
         }
         cout<<"Enter the element:";
         cin>>k;
-        flag=0;
-        for(int i=0;i<m;i++)
-        {
-            if(k==a[i])
-            {
-                flag=1;
-                cout<<"Present"<<i+ 1<<endl;
-                break;
-            }
-        }
-        if(flag==0)
-        cout<<"Not Present"<<m<<endl;
+        linear(a,m,k);
         n--;
     }
+}
+void linear(int a[],int m,int k){
+    int flag=0;
+    for(int i=0;i<m;i++)
+    {
+        if(k==a[i])
+        {
+            flag=1;
+            cout<<"Present "<<i+ 1<<endl;
+            break;
+        }
+    }
+    if(flag==0)
+        cout<<"Not Present "<<m<<endl;
 }
